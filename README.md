9a07ef546186732cfc70d9b4f5dd1cd4dddefce3
vhincent <>
2025-09-03 23:01:27 -0800

    readme

---
+++ README.md
@@ -0,0 +1,28 @@
+## My Blogspot in NodeJS
+### Startup
+Insert [API Key](https://developers.google.com/blogger/docs/3.0/using) in .env
+###### .env
+```
+BLOGGER_API_KEY=
+```
+[Get the ID of your Blog](https://support.google.com/blogger/thread/211636138?hl=en&msgid=211650454)
+###### app.config.js
+```
+blogger: {
+    ...
+    blogId: 'here'
+    ...
+}
+```
+Download your Blogspot with the command:
+```
+yarn run api:blogger export
+```
+convert it
+```
+yarn run api:blogger convert
+```
+then run
+```
+yarn run start
+```
--- src/app.js
+++ src/app.js
@@ -11,67 +11,53 @@ import routes from './routes/app.routes.js';
 import * as middlewares from './middleware/errorhandler.middleware.js';
 // color text
 import chalk from 'chalk';
-
 const app = express();
 const port = process.env.PORT;
 const __dirname = path.resolve();
-
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 // Serve static files (CSS, etc.)
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(compress());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
 // sessions
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false, // don't save session if unmodified
-  saveUninitialized: false, // don't create session until something stored
+    secret: process.env.SESSION_SECRET,
+    resave: false, // don't save session if unmodified
+    saveUninitialized: false, // don't create session until something stored
 }));
-
 app.use('/', routes);
-
 app.use((req, res, next) => {
-  // Get the remote IP address
-  let remoteIp = req.ip || req.connection.remoteAddress; // Use req.ip if trust proxy is enabled
-
-  // Store the IP address in the session if it's not already there
-  if (!req.session.remoteAddress) {
-    req.session.remoteAddress = remoteIp;
-  }
-
-  // You can also update it on subsequent requests if needed,
-  // for example, to track changes in IP address (e.g., mobile network changes)
-  // req.session.lastKnownRemoteAddress = remoteIp;
-
-  console.log(remoteIp);
-  next();
+    // Get the remote IP address
+    let remoteIp = req.ip || req.connection.remoteAddress; // Use req.ip if trust proxy is enabled
+    // Store the IP address in the session if it's not already there
+    if (!req.session.remoteAddress) {
+        req.session.remoteAddress = remoteIp;
+    }
+    // You can also update it on subsequent requests if needed,
+    // for example, to track changes in IP address (e.g., mobile network changes)
+    // req.session.lastKnownRemoteAddress = remoteIp;
+    console.log(remoteIp);
+    next();
 });
-
 // app.use(function(req, res, next) {
 //   res.status(404);
-
 //   // respond with html page
 //   if (req.accepts('html')) {
 //     res.render('404', { url: req.url });
 //     return;
 //   }
-
 //   // respond with json
 //   if (req.accepts('json')) {
 //     res.json({ error: 'Not found' });
 //     return;
 //   }
-
 //   // default to plain-text. send()
 //   res.type('txt').send('Not found');
 // });
-
 // 404 Not Found Middleware (ALWAYS keep this as the last route/middleware)
 // app.use((req, res, next) => {
 // respond with html page
@@ -79,18 +65,14 @@ app.use((req, res, next) => {
 //   res.render("error", { url: req.url });
 //   return;
 // }
-
 // // respond with json
 // if (req.accepts("json")) {
 //   res.json({ error: "Not found" });
 //   return;
 // }
-
 // // default to plain-text. send()
 // res.type("txt").send("Not found");
-
 // res.status(404).send("Sorry, the page you're looking for cannot be found!");
-
 // res.format({
 //   html: function () {
 //     // res.render("404", { url: req.url });
@@ -104,30 +86,26 @@ app.use((req, res, next) => {
 //     res.type("txt").send("Not found");
 //   },
 // });
-
 // return handleError(req,res, next)
 // });
-
 app.use(middlewares.notFound);
-
 // the error handler is placed after routes
 // if it were above it would not receive errors
 // from app.get() etc
 app.use(middlewares.errorHandler);
-
 const server = app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: ` + chalk.bgBlue.white(`http://localhost:${port}`));
-  /* eslint-enable no-console */
+    /* eslint-disable no-console */
+    console.log(`Listening: ` + chalk.bgBlue.white(`http://localhost:${port}`));
+    /* eslint-enable no-console */
 });
-
 server.on('error', (err) => {
-  switch (err.code) {
-    case 'EADDRINUSE':
-      console.error(`Port ${port} is already in use. Please choose another port or stop the process using it.`);
-    default:
-      console.error('Failed to start server:', err);
-      break;
-  }
-  process.exit(1);
+    switch (err.code) {
+        case 'EADDRINUSE':
+            console.error(`Port ${port} is already in use. Please choose another port or stop the process using it.`);
+        default:
+            console.error('Failed to start server:', err);
+            break;
+    }
+    process.exit(1);
 });
+//# sourceMappingURL=app.js.map
